50068f25737eeddf8e50c48df13674d19f519064
Binh Truong <binhtt.13122000@gmail.com>
2021-06-09 03:29:11 +0700

    add webpack

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { DefaultLabel } from '../../components/labels/DefaultLabel';
 
 export const App = (): React.ReactElement => {
   const [appName, setAppName] = React.useState<string>('');
